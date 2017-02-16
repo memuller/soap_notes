7624ba228781dc4af09843694fe5480d12d7c1d5
Matheus E. Muller <hello@memuller.com>
2017-02-16 02:34:29 -0200

    Assorted commit.

--- app.js
+++ app.js
@@ -1,4 +1,33 @@
-const app = angular.module('helloWorld', [])
-app.controller('helloWorldCtrl', ($scope) => {
-  $scope.message = "Hello World!"
+const app = angular.module('Phonebook', [])
+
+
+app.controller('PhonebookController', ($scope) => {
+  $scope.app = 'Lista Telefônica'
+  $scope.contacts = [
+    {name: 'Matheus', phone: 988535854, },
+    {name: 'Alline', phone: 72198321},
+    {name: 'Lucas', phone: 732891723}
+  ]
+  $scope.phoneOperators = [
+      {name: 'Oi', code: 14, category: 'Mobile'},
+      {name: 'Claro', code: 21, category: 'Mobile'},
+      {name: 'Tim', code: 15, category: 'Landline'}
+  ]
+  $scope.addContact = (contact) => {
+    console.log(contact)
+    $scope.contacts.push(contact)
+    delete $scope.contact
+  }
+  $scope.someSelected = (contacts) => {
+    return contacts.some((contact) => {
+      if(contact.selected)
+        return true
+    })
+  }
+  $scope.removeContact = (contacts) => {
+    $scope.contacts = contacts.filter((contact) =>{
+      if(!contact.selected)
+        return contact
+    })
+  }
 })
