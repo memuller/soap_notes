7af4978cb8b440d8d8ee73ac85da9c791be6e533
Matheus E. Muller <hello@memuller.com>
2017-02-20 10:52:33 -0300

    Adds simple patient list and layout.

--- app.js
+++ app.js
@@ -1,33 +1,10 @@
-const app = angular.module('Phonebook', [])
+const app = angular.module('SOAPNotes', [])
 
+app.controller('SOAPController', ($scope) => {
+  $scope.title = 'SOAP Notes'
 
-app.controller('PhonebookController', ($scope) => {
-  $scope.app = 'Lista Telefônica'
-  $scope.contacts = [
-    {name: 'Matheus', phone: 988535854, },
-    {name: 'Alline', phone: 72198321},
-    {name: 'Lucas', phone: 732891723}
+  $scope.patients = [
+    {name: 'lucas'}, {name: 'devard'}, {name: 'demostenes'}
   ]
-  $scope.phoneOperators = [
-      {name: 'Oi', code: 14, category: 'Mobile'},
-      {name: 'Claro', code: 21, category: 'Mobile'},
-      {name: 'Tim', code: 15, category: 'Landline'}
-  ]
-  $scope.addContact = (contact) => {
-    console.log(contact)
-    $scope.contacts.push(contact)
-    delete $scope.contact
-  }
-  $scope.someSelected = (contacts) => {
-    return contacts.some((contact) => {
-      if(contact.selected)
-        return true
-    })
-  }
-  $scope.removeContact = (contacts) => {
-    $scope.contacts = contacts.filter((contact) =>{
-      if(!contact.selected)
-        return contact
-    })
-  }
+
 })
