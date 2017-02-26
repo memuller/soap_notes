222558d47635bdee0bec3f7895dcc0522fb713a9
Matheus E. Muller <hello@memuller.com>
2017-02-25 22:18:59 -0300

    Converts Note service to a nice ES6 class.

--- services.js
+++ services.js
@@ -1,40 +1,51 @@
 /* # Notes Service
  * lists, creates, and saves Notes.
  */
-app.service('Notes', function(){
+app.factory('Note', function(){
 
-  // key which will be used for HTML5 localStorage .
-  this.keyName = 'notes'
+  class Note {
 
-  // returns all notes as an array (or an empty array, if none)
-  this.list = () => {
-    return JSON.parse(localStorage.getItem(this.keyName)) || []
-  }
+    // key at which notes will be stored
+    static keyName () { return 'notes' }
 
-  // returns a properly formatted note, ready for storage
-  this.create = (params) => {
-    const note = {
-      subjective:   params.subjective,
-      objective:    params.objective,
-      assessment:   params.assessment,
-      plan:         params.plan,
-      patientName:  params.patientName,
-      createdAt:    new Date()
+    // costructor.
+    // * id is an unique timestamp used as identifier
+    // * createdAt is a timestamp
+    // * all other attributes are textual, fetched from the given parameters
+    constructor (params) {
+      this.id =           (new Date()).valueOf()
+      this.subjective =   params.subjective
+      this.objective =    params.objective
+      this.assessment =   params.assessment
+      this.plan =         params.plan
+      this.patientName =  params.patientName
+      this.createdAt =    new Date()
     }
 
-    return note
-  }
+    // returns all notes as an array (or an empty array, if none)
+    static list () {
+      return JSON.parse(localStorage.getItem(this.keyName())) || []
+    }
 
-  // pushes a given note to storage
-  this.push = (note) => {
-    const notes = this.list()
-    notes.push(note)
-    localStorage.setItem(this.keyName, JSON.stringify(notes))
-  }
+    // pushes a given note to storage
+    static push (note) {
+      const notes = this.list()
+      notes.push(note)
+      localStorage.setItem(this.keyName(), JSON.stringify(notes))
+      return note
+    }
 
-  // creates and stores a note
-  this.save = (note) => {
-    this.push(this.create(note))
-  }
+    // creates and stores a note
+    static save (note) {
+      return this.push( new Note(note) )
+    }
 
+    // fetches a specific note, by id
+    static get (id) {
+      const notes = this.list()
+      return notes.find( (item) => item.id == id )
+    }
+
+  }
+  return Note
 })
