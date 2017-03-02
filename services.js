4bd9ed9701aa0184aa416b52e015cc3fb1b8ebc1
Matheus E. Muller <hello@memuller.com>
2017-03-01 21:32:06 -0300

    Improves storage service w/ a base class.

--- services.js
+++ services.js
@@ -1,51 +1,68 @@
-/* # Notes Service
- * lists, creates, and saves Notes.
+/* # StoredObject Class
+ * Handles storage of objects in the brower's LocalStorage.
+ * Works with an ORM-like API.
+ */
+ class StoredObject {
+
+   // keyname at which all objects will be stored under
+   static keyName() { return this.className }
+
+   // constructor:
+   // * sets all properties on given parameter as properties of this object;
+   // * sets object's id;
+   // * sets object's timestamp.
+   constructor (params){
+     for(let property in params){
+       this[property] = params[property]
+     }
+
+     this.id = (new Date()).valueOf()
+     this.createdAt = new Date()
+   }
+
+   // returns the list of all objects
+   static list(){
+     return JSON.parse(localStorage.getItem(this.keyName())) || []
+   }
+
+   // pushes a given object to storage
+   static push (object) {
+     const objects = this.list()
+     objects.push(object)
+     localStorage.setItem(this.keyName(), JSON.stringify(objects))
+     return object
+   }
+
+   // creates and stores an object
+   static save (object) {
+     return this.push( new this(object) )
+   }
+
+   // fetches a specific object, by id
+   static get (id) {
+     const objects = this.list()
+     return objects.find( (item) => item.id == id )
+   }
+ }
+
+/* # Notes service
  */
 app.factory('Note', function(){
 
-  class Note {
-
-    // key at which notes will be stored
-    static keyName () { return 'notes' }
-
-    // costructor.
-    // * id is an unique timestamp used as identifier
-    // * createdAt is a timestamp
-    // * all other attributes are textual, fetched from the given parameters
-    constructor (params) {
-      this.id =           (new Date()).valueOf()
-      this.subjective =   params.subjective
-      this.objective =    params.objective
-      this.assessment =   params.assessment
-      this.plan =         params.plan
-      this.patientName =  params.patientName
-      this.createdAt =    new Date()
-    }
-
-    // returns all notes as an array (or an empty array, if none)
-    static list () {
-      return JSON.parse(localStorage.getItem(this.keyName())) || []
-    }
-
-    // pushes a given note to storage
-    static push (note) {
-      const notes = this.list()
-      notes.push(note)
-      localStorage.setItem(this.keyName(), JSON.stringify(notes))
-      return note
-    }
-
-    // creates and stores a note
-    static save (note) {
-      return this.push( new Note(note) )
-    }
-
-    // fetches a specific note, by id
-    static get (id) {
-      const notes = this.list()
-      return notes.find( (item) => item.id == id )
-    }
+  class Note extends StoredObject {
 
   }
+
   return Note
+})
+
+/* # Patients service
+ */
+app.factory('Patients', function(){
+
+  class Patient extends StoredObject {
+
+  }
+
+  return Patient
 })
